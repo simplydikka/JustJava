20f73398e48c10dc20c8b9c8f3ca5d3c636aff4b
simplydikka <33466126+simplydikka@users.noreply.github.com>
2018-01-23 17:56:00 +0200

    Done for the day, added the CheckBox view with all correct values in the xml, up to the point where I have to make it work and store the state in a value to show as a message. I'm tired, need sleep.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,37 +9,38 @@
 package com.example.android.justjava;
 
 
-import android.content.Context;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
-import java.text.NumberFormat;
-import java.util.Currency;
-import java.util.Locale;
+
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    public static final double PRICE_PER_COFFEE = 5;
+    public static final int PRICE_PER_COFFEE = 5;
     int numberOfCoffees = 0;
-
+    boolean checkBoxValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         numberOfCoffees = numberOfCoffees + 1;
-        display(numberOfCoffees);
+        displayQuantity(numberOfCoffees);
     }
 
     /**
@@ -57,26 +58,54 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         numberOfCoffees = numberOfCoffees - 1;
-        display(numberOfCoffees);
+        displayQuantity(numberOfCoffees);
     }
 
     /**
-     * This method is called when the order button is clicked.
+     * This method is called when the order button is clicked. Shows the total price of the order and a message
      */
     public void submitOrder(View view) {
+        displayMessage(createOrderSummary());
 
-        displayPrice(numberOfCoffees * PRICE_PER_COFFEE);
+
+    }
+
+
+    /**
+     * Calculates the total price of the order and shows a summary of the order
+     *
+     * @return price and order summary
+     */
+    private String createOrderSummary() {
+        int amountDue = calculatePrice();
+        Log.v("Main Activity", "The price is " + amountDue);
+        String clientName = "Kaptain Kunal";
+        String priceMessage = "Name: " + clientName + "\nQyantity: " + numberOfCoffees + "\nTotal: " + amountDue + "лв.";
+        priceMessage += "\nThank You!";
+        return priceMessage;
+    }
+
+
+    /**
+     * Calculates the total price of the order.
+     *
+     * @return price
+     */
+    private int calculatePrice() {
+        return numberOfCoffees * PRICE_PER_COFFEE;
+
     }
 
     /**
-     * This method displays the given price on the screen.
+     * This method displays the given text on the screen.
      */
-    private void displayPrice(double number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        Currency curr = Currency.getInstance("BGN");
-        Locale bgLocale = new Locale("bg", "BG");
-        System.out.println(curr.getSymbol(bgLocale));
-        priceTextView.setText(NumberFormat.getCurrencyInstance(bgLocale).format(number));
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+
+
     }
+
+
 
 }
