7023e92a465995cf1859464706f36f30ba0b6b52
simplydikka <33466126+simplydikka@users.noreply.github.com>
2018-01-29 14:32:37 +0200

    Added the if/else statements, something is breaking, don't know what

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,20 +9,19 @@
 package com.example.android.justjava;
 
 
-import android.content.Context;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+
+
 import android.view.View;
-import android.widget.Button;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
-import java.text.NumberFormat;
-import java.util.Currency;
-import java.util.Locale;
+
 
 /**
  * This app displays an order form to order coffee.
@@ -30,7 +29,12 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     public static final int PRICE_PER_COFFEE = 5;
     int numberOfCoffees = 0;
-
+    CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+    boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+    CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+    boolean hasChocolate = chocolateCheckBox.isChecked();
+    EditText edit = (EditText) findViewById(R.id.name_text_input);
+    String clientName = edit.getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,15 +70,22 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked. Shows the total price of the order and a message
      */
     public void submitOrder(View view) {
-        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
-        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-        Log.v("MainActivity", "Has whipped cream: " + hasWhippedCream);
+
 
         displayMessage(createOrderSummary());
 
 
     }
 
+    /**
+     * hides the keyboard
+     */
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+    }
 
     /**
      * Calculates the total price of the order and shows a summary of the order
@@ -82,14 +93,14 @@ public class MainActivity extends AppCompatActivity {
      * @return price and order summary
      */
     private String createOrderSummary() {
+
         int amountDue = calculatePrice();
-        Log.v("Main Activity", "The price is " + amountDue);
-        String clientName = "Kaptain Kunal";
-        String availableTopings = "Add whipped cream?";
-        availableTopings += " " + "";
-        String priceMessage = "Name: " + clientName + "\n" + availableTopings + "\nQyantity: " + numberOfCoffees + "\nTotal: " + amountDue + "лв.";
-        priceMessage += "\nThank You!";
-        return priceMessage;
+        String summaryMessage = "Name: " + clientName;
+        summaryMessage += "\nAdd whipped cream? " + hasWhippedCream;
+        summaryMessage += "\nAdd chocolate? " + hasChocolate;
+        summaryMessage += "\nQuantity: " + numberOfCoffees + "\nTotal: " + amountDue + "лв.";
+        summaryMessage += "\nThank You!";
+        return summaryMessage;
     }
 
 
@@ -99,8 +110,17 @@ public class MainActivity extends AppCompatActivity {
      * @return price
      */
     private int calculatePrice() {
-        return numberOfCoffees * PRICE_PER_COFFEE;
 
+        int basePrice = PRICE_PER_COFFEE;
+
+        if (whippedCreamCheckBox.isChecked()) {
+            basePrice = (PRICE_PER_COFFEE + 1);
+        }
+        if (chocolateCheckBox.isChecked()) {
+            basePrice = (PRICE_PER_COFFEE + 2);
+        }
+
+        return numberOfCoffees * basePrice;
     }
 
     /**
@@ -114,5 +134,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
 }
+
+
+
+
